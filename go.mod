babb21dc6340f70d80a62208c8e2f993d8b4d461
Naoki Matsumoto <naoki@pibvt.net>
2021-02-05 15:36:54 +0900

    Add Dockerfile for test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/naoki9911/CREBAS
+
+go 1.13
---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT_DIR=$(cd $(dirname $0); pwd)
+echo $PROJECT_DIR
+
+sudo docker build -t crebas-test test/docker/
+if [ $? -ne 0 ]; then
+    echo "Failed to build test container"
+    exit 1
+fi
+
+sudo docker run -v $PROJECT_DIR:/CREBAS --rm crebas-test
